e96e7a406cb5e6d970603ddffe9eb22b483ac87f
root <root@localhost.localdomain>
2019-06-20 23:56:42 +0200

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 docker build -t moatef/multi-client:latest -t moatef/multi-client:$SHA -f ./client/Dockerfile.dev ./client
 docker build -t moatef/multi-server:latest -t moatef/multi-server:$SHA -f ./server/Dockerfile.dev ./server
 docker build -t moatef/multi-worker:latest -t moatef/multi-worker:$SHA -f ./worker/Dockerfile.dev ./worker
-docker push moatef/multi-client
-docker push moatef/multi-server
-docker push moatef/multi-worker
+docker push moatef/multi-client:latest
+docker push moatef/multi-server:latest  
+docker push moatef/multi-worker:latest
+docker push moatef/multi-client:$SHA
+docker push moatef/multi-server:$SHA  
+docker push moatef/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=moatef/multi-server
+kubectl set image deployments/server-deployment server=moatef/multi-server:$SHA
+kubectl set image deployments/client-deployment client=moatef/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=moatef/multi-worker:$SHA
